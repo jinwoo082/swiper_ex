4a820b1128fad8ecc50113194cd39002fb92ee46
jinwoo082 <wistjd5791@naver.com>
2022-01-06 02:29:19 +0900

    step3 스와이프 완성

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-const swiper = new Swiper(".swiper", {});
+const swiper = new Swiper(".swiper", {
+  speed: 500,
+  loop: true,
+  direction: "horizontal",
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    // clickable: true,
+    type: "fraction",
+  },
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: -100,
+    // depth: 400,
+    slideShadows: false,
+  },
+});
